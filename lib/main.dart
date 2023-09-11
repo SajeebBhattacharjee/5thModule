46c79b9bb40bdc064ef1b0424bc9fda33b54bd12
SajeebBhattacharjee <144474919+SajeebBhattacharjee@users.noreply.github.com>
2023-09-11 17:15:00 +0600

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,92 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main(){
+  runApp(MyApp());
+}
 
-    )
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
 
-  );
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: HomeScreen(),
+      title: 'Ostad APP',
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        backgroundColor: Colors.white,
+
+        appBar: AppBar(
+          toolbarHeight: 100,
+          elevation: 70,
+          leading: Icon(Icons.store, size: 36),
+          actions: [
+            Icon(Icons.search, size: 36)
+          ],
+          centerTitle: true,
+          title: Text('Home', style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 26
+          ),),
+          backgroundColor: Colors.green,
+
+        ),
+
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('This is mod 5 Assignment', style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+                ),)
+
+              ],
+
+            ),
+
+            RichText(text: TextSpan(
+                text: 'My ',
+                style: TextStyle(
+                    color: Colors.pink,
+                    fontSize: 22
+                ),
+
+                children: [
+                  TextSpan(
+                      text: 'phone ',
+                      style: TextStyle(
+                          color: Colors.blueAccent,
+                          fontSize: 16
+                      )
+                  ),
+                  TextSpan(
+                      text: 'name ',
+                      style: TextStyle(
+                          color: Colors.purpleAccent,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold
+                      )
+                  ),
+                  TextSpan(
+                      text: 'Your phone name',
+                      style: TextStyle(
+                          color: Colors.orange
+                      )
+                  )
+                ]
+            ))
+          ],
+        )
+    );
+  }
 }
